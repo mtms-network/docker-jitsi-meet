a8c4cd45071b30d79d277f9f118411bf1ab2eebb
paoduy <paoduy@gmail.com>
2024-05-19 00:23:57 +0700

    update logo

--- up.sh
+++ up.sh
@@ -2,4 +2,4 @@ git pull
 docker compose down
 cp ./env.mtms ./.env
 docker compose -f docker-compose.yml -f jigasi.yml -f jibri.yml -f etherpad.yml up -d
-docker cp ./mtms/config.js web:/config/config.js
+docker cp ./mtms/config.js docker-jitsi-meet-web-1:/config/config.js
