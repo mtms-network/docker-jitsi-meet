22765812a3bacc533397167295a6f87f75ad7869
paoduy <paoduy@gmail.com>
2024-09-07 13:17:16 +0700

    update recordings

--- up.sh
+++ up.sh
@@ -1,5 +1,5 @@
 git pull
-docker compose down
+docker compose down --remove-orphans
 cp ./env.mtms ./.env
 docker compose -f docker-compose.yml -f jigasi.yml -f jibri.yml -f etherpad.yml up -d
 docker cp ./mtms/config.js docker-jitsi-meet-web-1:/config/config.js
